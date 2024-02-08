2e4cd6e15a50b0f8f4d89b91a4d6ec507586ef3c
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2024-02-08 20:28:59 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,17 @@
+    }
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+    return data;
+}
+function generateRandomData() {
+
+    const data = generateRandomData();
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+        console.log("Random Number: " + item);
+
+    data.forEach(item => {
+main();
+
+    });
